4cbe7708c39dfb2bc8bd658b7ed90ae820318cbd
Gab <gab.despres@gmail.com>
2021-04-15 08:33:12 -0400

    Fixed dependencies, improved doc

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as fh:
 
 setuptools.setup(
     name="insightlab",
-    version="0.2",
+    version="0.3",
     author="IDLab",
     author_email="it-admin@idlab.org",
     description="Library for interacting with Jira Cloud Insight API",
     keywords="insight jira-cloud",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    python_requires=">=3.7",
+    python_requires=">=3.4",
     url="https://github.com/idlab-org/insightlab",
     packages=["insightlab"],
     classifiers=["Programming Language :: Python :: 3"],
